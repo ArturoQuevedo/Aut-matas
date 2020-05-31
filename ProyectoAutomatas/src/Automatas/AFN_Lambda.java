10ca2a48c275f3ce08df558be05aa59d753cf297
Nicolas Sanchez <nisanchezva@unal.edu.co>
2020-05-31 13:20:37 -0500

    Metodos de lambda clausura hechos

--- ProyectoAutomatas/src/Automatas/AFN_Lambda.java
+++ ProyectoAutomatas/src/Automatas/AFN_Lambda.java
@@ -13,6 +13,9 @@ public class AFN_Lambda {
         private String q;
         private ArrayList<String> finalStates;
         private ArrayList<String>[][] delta;
+        
+        
+         
 
         public Automata() {
             this.sigma = new ArrayList<>();
@@ -156,7 +159,7 @@ public class AFN_Lambda {
          
 
         int getPosEstado(String estado){
-            int val = 0;
+            int val = 0; // aqui se asignan posiciones a estados que no existen, posible solución es hacer val = -1 pero no se si afecte el codigo de cesar
             for(int i=0;i<this.states.size();i++){
 //                System.out.println(this.states.get(i));
                 if(estado.equals(this.states.get(i))){
@@ -169,7 +172,7 @@ public class AFN_Lambda {
         
         int getPosSimbolo(String simbolo){
             int val = 0;
-            for(int i=0;i<this.states.size();i++){
+            for(int i=0;i<this.sigma.size();i++){
 //                System.out.println(this.states.get(i));
                 if(simbolo.equals(Character.toString(this.sigma.get(i)))){
                     val = i;
@@ -359,6 +362,110 @@ public class AFN_Lambda {
             }
             return aceptada;
         }
+        
+        ArrayList<String> calcularLambdaClausura(String estado){
+        
+            ArrayList<String> visitedStates = new ArrayList<>();//Los estados que ya visitamos para evitar caer en bucles
+            ArrayList<String> statesToVisit = new ArrayList<>();//los estados que tenemos que visitar (solo entran los que no estan en el array de arriba)
+            ArrayList<String> antiMultiplesVisitas = new ArrayList<>(); // esto evitara que un mismo lugar se visite varias veces (es complicado explicarlo aqui entonces preguntenme plox :v (A Nicolas))
+            for(int i = 0;i < this.states.size();i++){
+            antiMultiplesVisitas.add(this.states.get(i));
+            }
+           
+            statesToVisit.add(estado);
+            visitedStates.add(estado); // esto lo hago para que la función no tenga problema iniciando, al final del codigo se quita
+            
+            while(!statesToVisit.isEmpty()){
+                
+                int statePosition  = getPosEstado(statesToVisit.get(0));
+                int symbolPosition = getPosSimbolo("$");
+                
+                if(this.delta[statePosition][symbolPosition].isEmpty()){
+                //en el caso de que no vaya hacia ningun lado con lambda
+                    visitedStates.add(statesToVisit.remove(0));
+             
+                }else{
+                    //verificando que el estado que se quiere agregar no se haya visitado antes
+                    
+                    for(int i = 0; i < this.delta[statePosition][symbolPosition].size(); i++){
+                  
+                        for(int j = 0; j < visitedStates.size();j++ ){
+                            
+                            if(this.delta[statePosition][symbolPosition].get(i).equals(visitedStates.get(j))){
+                                
+                                break;
+                                
+                            }
+                            
+                            if(j == visitedStates.size()-1){
+                                // si llego hasta aqui es por que el estado no se ha visitado hasta el momento entonces se agrega
+
+                                if(antiMultiplesVisitas.contains(this.delta[statePosition][symbolPosition].get(i))){
+                                    
+                                    statesToVisit.add(this.delta[statePosition][symbolPosition].get(i)); // esto es para evitar multiples visitas debido a que visitedStates se actualiza lento 
+                                    antiMultiplesVisitas.remove(this.delta[statePosition][symbolPosition].get(i));
+                                    
+                                }
+                                
+                                
+                                
+                            }
+                           
+                        }
+                   
+                    }
+                //pasando el estado que se acaba de visitar a la lista de visitados y quitandolo de los estados a visitar
+                    
+                visitedStates.add(statesToVisit.remove(0));
+                
+                }
+            
+            }
+        visitedStates.remove(0); // aqui se quita lo que añadi al inicio 
+        
+        
+        
+        
+        return visitedStates;
+        
+        };
+        
+        ArrayList<String> printLambdaClausura(String estado){
+        
+            ArrayList<String> lambdaClausura = new ArrayList<>();
+            lambdaClausura = calcularLambdaClausura(estado);
+            Collections.sort(lambdaClausura);
+            
+            System.out.print("La Lambda Clausura del estado " + estado +" es : "+"[ " );
+            
+            for(int i = 0; i < lambdaClausura.size()-1;i++){
+                
+                System.out.print(lambdaClausura.get(i) + ", ");
+                
+            }
+            System.out.print(lambdaClausura.get(lambdaClausura.size()-1));//esto es sencillamente par aque no quede una coma suelta al final :V
+            
+            System.out.print(" ]\n");
+            return lambdaClausura;
+            
+        }
+        
+        ArrayList<ArrayList<String>> calcularMuchasLambdaClausura (ArrayList<String> estados){
+        
+        ArrayList<ArrayList<String>> muchasLambdaClausuras = new ArrayList<>();
+        
+        for(int i = 0; i < estados.size();i++){
+            
+            muchasLambdaClausuras.add(printLambdaClausura(estados.get(i)));
+            
+        }
+        
+        
+        
+        return muchasLambdaClausuras;
+        }
+        
+        
     }   
     
     public static void main(String[] args) throws Exception {
@@ -370,10 +477,16 @@ public class AFN_Lambda {
         afd.showInitialState();
         afd.showFinalStates();
         afd.showDelta();
-        boolean resultado = afd.procesarCadena("babaabb");
+//boolean resultado = afd.procesarCadena("babaabb");
 //        boolean resultado = afd.procesarCadena("b");
 //
-        System.out.println(resultado);
+        //afd.printLambdaClausura("s4");
+        ArrayList<String> estados = new ArrayList<>();
+        estados.add("s1");
+        estados.add("s2");
+        estados.add("s3");
+        afd.calcularMuchasLambdaClausura(estados);
+        
         
     }
 
