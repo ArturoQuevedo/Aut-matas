f040c86a99d7fb2018317f814432d60856c88a1a
Rhiino7 <36159485+Rhiino7@users.noreply.github.com>
2020-06-21 01:06:43 -0500

    Punto 10 a medias.

--- ProyectoAutomatas/src/Automatas/Temporal_AFN_LambdaToAFN.java
+++ ProyectoAutomatas/src/Automatas/Temporal_AFN_LambdaToAFN.java
@@ -126,14 +126,177 @@ public class Temporal_AFN_LambdaToAFN {
     };
     
     
+    public static ArrayList<String> obtenerEstadosAccesibles(AFD afdinput) {
+
+        ArrayList<String> accesibles = new ArrayList<>();
+
+        for (int j = 0; j < afdinput.getDelta().length; j++) {
+            String estadoActual = afdinput.getStates().get(j);
+            for (int k = 0; k < afdinput.getDelta()[j].length; k++) {
+                if (afdinput.getDelta()[j][k].isEmpty()) {
+                    continue;
+                } else {
+                    for (String transicion : afdinput.getDelta()[j][k]) {
+                        if (!estadoActual.equals(transicion)) {
+                            if (!accesibles.contains(transicion)) {
+                                accesibles.add(transicion);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Collections.sort(accesibles);
+        for (int i = 0; i < accesibles.size(); i++) {
+            System.out.print(accesibles.get(i) + "\n ////// \n");
+        }
+        return accesibles;
+    }
+    
+    public static AFD modificarAutomata(AFD afdinput, ArrayList<String> accesibles, ArrayList<String> inaccesibles){
+       
+        for (int i = 0; i < afdinput.getStates().size(); i++) {
+            if (!accesibles.contains(afdinput.getStates().get(i))) {
+                inaccesibles.add(afdinput.getStates().get(i));
+            }
+        }
+        
+        for (int i = 0; i < afdinput.getStates().size(); i++) {
+            if(inaccesibles.contains(afdinput.getStates().get(i))){
+                for (int j = 0; j < afdinput.getSigma().size(); j++) {
+                    afdinput.getDelta()[i][j] = null;
+                }
+            }
+        }
+       
+        
+        return afdinput;
+    }
+    
+    public static String[][] simplificacionAutomataAFD(AFD afdinput, ArrayList<String> accesibles){
+        String[][] triangular = new String[accesibles.size()][accesibles.size()];
+        
+        for (int i = 0; i < accesibles.size(); i++) {
+            for (int j = i+1; j < accesibles.size(); j++) {
+                String p = accesibles.get(j);
+                boolean pAceptacion = afdinput.getFinalStates().contains(p);
+                String q = accesibles.get(i);
+                boolean qAceptacion = afdinput.getFinalStates().contains(q);
+                if(!(pAceptacion == qAceptacion)){
+                    triangular[i][j] = "1";
+                }else
+                    triangular[i][j] = "0";
+            }
+        }
+        
+        for(int i=0;i<triangular.length;i++){
+            for(int j=i+1;j<triangular.length;j++){
+                System.out.println(triangular[i][j]);
+            }
+        }
+        simplificacionAutomataAFD2(afdinput, triangular, accesibles);
+        return triangular;
+    }
+    
+    public static void simplificacionAutomataAFD2(AFD afdinput, String[][]triangular, ArrayList<String> accesibles){
+        ArrayList<Integer> posiciones = triangularAEstados(afdinput, triangular, accesibles);
+        ArrayList<Integer> copyPos = (ArrayList<Integer>) posiciones.clone();
+        
+        ArrayList<String>[][] tabular = new ArrayList[posiciones.size()/2][afdinput.getSigma().size()];
+        
+        int indice = 0;
+        int k = 0;
+        while(!copyPos.isEmpty()){
+            int posEstadoP = copyPos.remove(0);
+            int posEstadoQ = copyPos.remove(0);
+            k = 0;
+             for (int j = 0; j < afdinput.getSigma().size(); j++) {
+                String vaP = afdinput.getDelta()[posEstadoP][j].get(0); //SE PUEDE MORIR GONORREA POR SER NULL, OJO CUIDAO
+                String vaQ = afdinput.getDelta()[posEstadoQ][j].get(0);
+
+                tabular[indice][k] = new ArrayList();
+                tabular[indice][k].add(vaP);
+                tabular[indice][k].add(vaQ);
+                k++;
+                if(k==2)
+                    indice++;
+            }
+        }
+        
+        for(int i = 0;i<tabular.length;i++){
+            for(int j = 0;j<tabular[i].length;j++){
+                if(!(tabular[i][j] == null))
+                for (String string : tabular[i][j]) {
+                    System.out.print(string + ",");
+                }
+                System.out.println("");
+            }
+        }
+        
+//        int indiceTabularI = 0;
+//        int indiceTabularJ = 0;
+//        for (int i = 0; i < triangular.length; i++) {
+//            indiceTabularI = i;
+//            indiceTabularJ = 0;
+//            for (int j = i+1; j < triangular.length; j++) {
+//                for (String string : tabular[indiceTabularI][indiceTabularJ]) {
+//                    System.out.print(string + ",");
+//                }
+//                
+//                
+//                String p = accesibles.get(j);
+//                boolean pAceptacion = afdinput.getFinalStates().contains(p);
+//                String q = accesibles.get(i);
+//                boolean qAceptacion = afdinput.getFinalStates().contains(q);
+//                if(!(pAceptacion == qAceptacion)){
+//                    triangular[i][j] = "1";
+//                }else
+//                    triangular[i][j] = "0";
+//                
+//                indiceTabularJ++;
+//            }
+//        }
+        
+    }
+    
+    public static ArrayList<Integer> triangularAEstados(AFD afdinput, String[][]triangular, ArrayList<String> accesibles){
+        ArrayList<Integer> posiciones = new ArrayList<>();
+        for(int i=0;i<triangular.length;i++){
+            for(int j=i+1;j<triangular.length;j++){
+                if(triangular[i][j].equals("0")){
+                    int indexP = afdinput.getStates().indexOf(accesibles.get(j));
+                    int indexQ = afdinput.getStates().indexOf(accesibles.get(i));
+                    System.out.println("index P = " + indexP + " index Q = " + indexQ);
+                    posiciones.add(indexP);
+                    posiciones.add(indexQ);
+                }
+            }
+        }
+        
+        return posiciones;
+    }
     
     
     public static void main(String[] args) throws Exception {
         
-        AFN_Lambda afnl = new AFN_Lambda();// eso no es un "uno" es una "l" :v
-        afnl.initializeAFD("AFN_Lambda2.txt"); // Aqui se debe poner el nombre del archivo que se desea leer
-        AFN_LambdaToAFN(afnl);
+//        AFN_Lambda afnl = new AFN_Lambda();// eso no es un "uno" es una "l" :v
+//        afnl.initializeAFD("AFN_Lambda2.txt"); // Aqui se debe poner el nombre del archivo que se desea leer
+//        AFN_LambdaToAFN(afnl);
+
+
+        AFD afd1 = new AFD();
+        afd1.initializeAFD("AFD1.txt");
+               
+        afd1.showDelta();
+        ArrayList<String> accesibles = obtenerEstadosAccesibles(afd1);
         
+        afd1 = modificarAutomata(afd1, accesibles, new ArrayList<>());
+        
+//        afd1.showStates();
+//        afd1.showDelta();
+
+        simplificacionAutomataAFD(afd1, accesibles);
+//        simplificacionAutomataAFD2(afd1, triangular, accesibles);
 
     }
     
